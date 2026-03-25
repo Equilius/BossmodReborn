2fe9420ca8c41d04375a7e78f5088977fbc75204
psyec4 <Equiliuschamberlain@gmail.com>
2026-03-25 13:01:55 +0000

    Fix: text hints for pari's curse

--- BossMod/Modules/Dawntrail/Criterion/C01AMT/C013PariOfPlenty/C013PariOfPlenty.cs
+++ BossMod/Modules/Dawntrail/Criterion/C01AMT/C013PariOfPlenty/C013PariOfPlenty.cs
@@ -4,14 +4,12 @@
 //  - Complete timeline - end of fight - need enrage log
 //  - Pari's Curse - Add stack/spread AOEs?
 //  - Towers add cleaves
-//  - Improve text hints on some mechanics
 //  - FireFlight:
 //        - find out who gets the stack
 //        - Add Enums value for left and right instead of using side number int
 //  - WheelOfFableFlight:
 //      - find out who gets the stack
 //      - Add initial AOE for the cleaves
-//  - Fix hints for witch hunt - currently says "Stack" "Bait" etc at the same time - most likely just disable for now until priority is added
 //  - Improve visual for SpurningFlames? - Really not needed, but could add indictors on the direction to go instead of showing the AOEs (assumes BD)
 //  - Pari's Curse - Mechanic works fine, just a lot of stuff for solving it - Remove gridMap if possible and find a different way to do it
 
